5e18ddf0e3d389e6e724ac6e5bf5cf87bae53dc9
Shivam <choudharyshivam0508@gmail.com>
2026-07-02 15:02:26 +0530

    Add user registration and authentication middleware
    - Implement user registration controller
    - Create JWT verification middleware
    - Set up user routes in the application

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,15 @@
-import express from 'express';
+import express, { urlencoded } from 'express';
+import cors from 'cors'
 
 const app = express();
+
+app.use(cors());
+
+app.use(express.json({limit : '16kb'}));
+app.use(urlencoded());
+
+import userRouter from './routes/user.route.js'
+
+app.use('/api/v1/user' , userRouter);
 
 export default app;
---
+++ server/src/middleware/auth.middleware.js
@@ -0,0 +1,8 @@
+import jwt from 'jsonwebtoken';
+
+import { asyncHandler } from '../utils/asyncHandler.js';
+import { ApiError } from '../utils/ApiError.js';
+
+export const verifyJWT = asyncHandler(async (req , res) => {
+
+});
